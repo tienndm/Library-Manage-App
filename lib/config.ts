5512a5c7dd8dc21d6253b7cec311706f91801487
tienndm <tien.nguyen@altek.com.vn>
2025-02-28 08:44:59 +0700

    feat: add speed inside monitor

--- lib/config.ts
+++ lib/config.ts
@@ -1,6 +1,7 @@
 export const config = {
     env: {
         apiEnpoint: process.env.NEXT_PUBLIC_API_ENPOINT,
+        prodApiEndpoint: process.env.NEXT_PUBLIC_PROD_API_ENPOINT,
         imagekit: {
             publicKey: process.env.NEXT_PUBLIC_IMAGEKIT_PUBLIC_KEY,
             urlEndpoint: process.env.NEXT_PUBLIC_IMAGEKIT_URL_ENPOINT,
